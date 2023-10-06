9652b8c1c31d0e99a688c56535e81bd2d285138e
Matthew Ferraro <matt@useyardstick.com>
2023-10-06 09:56:53 -0400

    create the Field class

---
+++ README.md
@@ -0,0 +1,13 @@
+# Stock Simulator
+
+This is an experiment to see if I can write a simulator that allows us to better understand our stock estimation performance as a function of different parameters.
+
+# To Run
+
+```bash
+python3 -m venv venv
+. venv/bin/activate
+pip install -r requirements.txt
+
+python simulator.py
+```
--- simulator.py
+++ simulator.py
@@ -1,5 +1,29 @@
+import numpy as np
+
+
+class Field:
+    def __init__(self, width_meters, height_meters, depth_meters, cell_resolution_meters):
+        self.width = width_meters
+        self.height = height_meters
+        self.depth = depth_meters
+        self.cell_resolution = cell_resolution_meters
+
+        self.cells = np.zeros(
+            (int(self.width / self.cell_resolution),
+             int(self.height / self.cell_resolution),
+             int(self.depth / self.cell_resolution)))
+
+
 def main():
-    print("hi")
+    field_width_meters = 100
+    field_height_meters = 100
+    field_depth_meters = 1
+    cell_resolution_meters = 0.1
+
+    field = Field(field_width_meters, field_height_meters,
+                  field_depth_meters, cell_resolution_meters)
+
+    print("Field cells: ", field.cells.shape)
 
 
 if __name__ == "__main__":
